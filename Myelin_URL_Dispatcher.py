58e2b7403fd406cfa805db30e7b141fd903e5852
Paul Gamble <paulgamble@Hippocraticai-Paul-Gamble.local>
2023-12-11 01:44:23 -0800

    first

---
+++ Myelin_URL_Dispatcher.py
@@ -0,0 +1,49 @@
+import streamlit as st
+from firebase.firebase_utils import write_task_item
+from task_url_sampler import sample_task_url
+
+
+FIREBASE_DB = "12-11-calls"
+
+active_task_names = [
+    "-", # Empty
+    "Medical Challenge Calls",
+    "Lab Engine Calls"
+]
+
+st.title('Myelin Task Dispatcher')
+
+with st.form("task-form"):
+
+    selected_task = st.selectbox("Please select your task:", active_task_names, key="task_name")
+
+    submitted = st.form_submit_button("Generate URL")
+
+    if submitted:
+        st.write("You selected:", selected_task)
+        # here's where I generate and display the URL
+        call_url = sample_task_url(selected_task)
+        st.subheader("[CALL URL]({})".format(call_url))
+
+with st.form("call-id-input"):
+    def log_call():
+        feedback_obj = {
+            "call_id": st.session_state.call_id,
+            "username": st.session_state.username,
+            "task_name": st.session_state.task_name,
+        }
+        write_task_item(feedback_obj, FIREBASE_DB)
+        #reset call_id
+        st.session_state.call_id = ""
+        #st.experimental_rerun()
+    
+    # call_url, call_type = sample_call_url()
+
+    st.text_input("Enter your name:", key='username')
+    # st.subheader("[CALL URL]({})".format(call_url))
+    #st.write("Call type: " + call_type.strip())
+    #st.write("After a few openings lines and introductions, please ask a hospital policy question from the provided list.")
+    call_id = st.text_input("Please copy and paste the call ID:", key="call_id")
+    
+    submit_button = st.form_submit_button(label='Log call', on_click=log_call)
+        
---
+++ task_url_sampler.py
@@ -0,0 +1,35 @@
+import glob
+import random
+import json
+
+TASK_CONFIGS = {
+    "Medical Challenge Calls" : {
+        "task_dir": "./task_configs/medical_challenge_calls_12_11/"
+    },
+    "Lab Engine Calls" : {
+        "task_dir": "./task_configs/lab_engine_calls_12_11/"
+    }
+
+}
+
+def sample_call_url(call_config_dir):
+    call_configs = glob.glob(call_config_dir)
+    call_config_fn = random.choice(call_configs)
+    with open(call_config_fn) as f:
+        call_config = json.load(f)
+    call_cfg = random.choice(call_config)
+    call_url = call_cfg["url"]
+    #call_type = call_config_fn.split("/")[-1][5:-5]
+    #call_type = call_type.replace("_", " ")
+    return call_url
+
+
+
+def sample_task_url(task_name):
+    """Sample a URL from the task config."""
+    task_config = TASK_CONFIGS[task_name]
+    task_dir = task_config["task_dir"]
+    call_url = sample_call_url(task_dir + "*.json")
+    
+    return call_url
+
