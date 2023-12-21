cacb4a30a70a8f3763f286ab1adf28f5afe2152a
Paul Gamble <paulgamble@Hippocraticai-Paul-Gamble.local>
2023-12-21 07:30:53 -0800

    kickout calls

--- task_url_sampler.py
+++ task_url_sampler.py
@@ -9,9 +9,9 @@ TASK_CONFIGS = {
     # "Lab Engine Calls" : {
     #     "task_dir": "./task_configs/lab_engine_calls_12_11/"
     # }
-    # "Kickout Calls 12/19" : {
-    #     "task_dir": "./task_configs/kickouts_12_19/"
-    # },
+    "Kickout Calls 12/21" : {
+        "task_dir": "./task_configs/kickouts_12_19/"
+    },
     # # "RAG Mentoring Calls" : {
     # #     "task_dir": "./task_configs/rag_mentoring_calls_12_13/"
     # # },
