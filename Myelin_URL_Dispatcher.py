8829a3c66a3db0ffe2675dbc6c2c7b1f002c7fb6
Paul Gamble <paulgamble@Hippocraticai-Paul-Gamble.local>
2024-01-04 09:55:33 -0800

    new fb db

--- Myelin_URL_Dispatcher.py
+++ Myelin_URL_Dispatcher.py
@@ -3,7 +3,7 @@ from firebase.firebase_utils import write_task_item
 from task_url_sampler import sample_task_url, TASK_CONFIGS
 
 
-FIREBASE_DB = "1-1-lab-calls"
+FIREBASE_DB = "1-4-calls"
 
 active_task_names = ["-"] + list(TASK_CONFIGS.keys())
 
