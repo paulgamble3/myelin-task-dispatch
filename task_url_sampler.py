c1a66d40116b608d8f02f13223d5a8adc1564a07
Paul Gamble <paulgamble@HippocrulGamble.attlocal.net>
2024-01-26 22:54:50 -0800

    rewrites

--- task_url_sampler.py
+++ task_url_sampler.py
@@ -9,9 +9,9 @@ TASK_CONFIGS = {
     # "Kickout End-to-End" : {
     #     "task_dir": "./task_configs/1_18_kickout_calls/"
     # },
-    "All Engine Mentoring Calls - 1/26" : {
+    "Mentoring Rewrites - 1/27" : {
         #"task_dir": "./task_configs/1_24_all_engine/"
-        "task_dir": "./task_configs/1_26_mentoring/"
+        "task_dir": "./task_configs/1_27_mentoring_rewrites/"
     }
     }
 
