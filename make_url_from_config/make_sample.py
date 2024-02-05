84baf6697ad1f3e2f9d160cceb7f15691cead18e
Paul Gamble <paulgamble@Hippocraticai-Paul-Gamble.local>
2024-02-05 11:40:17 -0800

    fix urls

--- make_url_from_config/make_sample.py
+++ make_url_from_config/make_sample.py
@@ -13,7 +13,7 @@ def url_from_config(config_dict):
 # mentoring calls
 # 
 
-config = {
+config_1 = {
   "model": "gpt4",
   "method": "voip",
   "supportEngines": [
@@ -96,6 +96,180 @@ config = {
 }
 
 
-url = url_from_config(config)
+config_2 = {
+  "model": "gpt4",
+  "method": "voip",
+  "supportEngines": [
+    "dob-verification-engine",
+    "identity-verification-engine",
+    "speaker-engine",
+    "kickout-engine",
+    "lab-engine",
+    "medication-engine"
+  ],
+  "template": "neel_v2",
+  "asrService": "dg-nova-2-medical",
+  "scriptId": "ac694643-8a5f-49f4-b0f5-c29d024e821c",
+  "nurseId": "ed9ca802-fb12-46d8-8298-dae2d2e7f81c",
+  "patientId": "108fd1f2-b8cd-45a7-ad19-c4d390615819",
+  "kickoutId": "b8b2a8be-47ee-4562-bb93-d07ee97ea546",
+  "ttsStreaming": False,
+  "llmStreaming": True,
+  "uiSettings": {
+    "showTasks": True,
+    "showDebug": False,
+    "showSystemPrompts": False,
+    "showVotes": True,
+    "showFeedback": True,
+    "showUserAudio": True
+  },
+  "otherSettings": {
+    "GOD_MODE_ENABLED": True,
+    "USE_LLAMA_SIMPLE_PROMPT_FORMAT": False,
+    "TURN_OFF_CONVERSATION_ENGINE": False,
+    "USE_DYNAMIC_CHEAT_SHEET": False,
+    "USE_SUMMARIZATION": True,
+    "USE_SUMMARY_FOLLOW_UP_IN_KICKOUTS": False,
+    "RAG_EMBEDDING_INDEX_NAME": "facilities-curated",
+    "USE_TOPIC_DETECTION": False,
+    "ELEVEN_LABS_STREAMING_LATENCY": 0,
+    "ELEVEN_LABS_MODEL_ID": "eleven_monolingual_v1",
+    "GOOGLE_ASR_MAX_AGGREGATE": 2,
+    "VOICE_FLOOR_RUN_LOCALLY": True,
+    "VOICE_FLOOR_RUN_TIMEOUT": 0.02,
+    "VOICE_FLOOR_VOLUME": 0,
+    "REMOVE_SPEAKER_UTTERANCE_THRU_VERIFICATION": True,
+    "SPEAKER_VERIFICATION_THRESHOLD_HIGH": 0.02,
+    "SPEAKER_VERIFICATION_THRESHOLD_LOW": 0.01,
+    "SPEAKER_VERIFICATION_NUM_FRAMES": 50,
+    "ASR_CONFIDENCE_STRATEGY": "words",
+    "ASR_CONFIDENCE_THRESHOLD": 0,
+    "SINGLE_WORD_ASR_CONFIDENCE_THRESHOLD": 0.6,
+    "SINGLE_WORD_ASR_CONFIDENCE_THRESHOLD_WHILE_SPEAKING": 0.75,
+    "ENABLE_TTS_CACHE": True,
+    "VAD_THRESHOLD": 0.95,
+    "VAD_CONTENT_BASED_TIME": 2000,
+    "VAD_VERIFICATION_TIME": 250,
+    "DUPLICATE_UTTERANCE_DETECTION_THRESHOLD": 0.9,
+    "GC_KEEP_IN_LAST_N_TURNS": 7,
+    "SUPPRESS_LABMED_DURING_KICKOUTS": False,
+    "SUPPRESS_LABS_AFTER_LAB_SECTION": False,
+    "IDENTITY_ENGINE_MODEL_NAME": "llama",
+    "DOB_ENGINE_MODEL_NAME": "llama-dob-model",
+    "KICKOUT_DETECTOR_MODEL_NAME": "kickout-detector",
+    "KICKOUT_EVALUATOR_MODEL_NAME": "kickout-evaluator",
+    "KICKOUT_FOLLOW_UPS_MODEL_NAME": "kickout-follow-up-evaluator",
+    "MEDICAL_DETECTOR_MODEL_NAME": "llama-medication-detector",
+    "MEDICAL_DOSAGE_EVALUATOR_MODEL_NAME": "llama-medication-dosage-evaluator",
+    "DRUG_ASR_DETECTOR_MODEL_NAME": "llama-drug-asr-detector",
+    "NUTRITION_DETECTOR_MODEL_NAME": "llama-nutrition-detector",
+    "LAB_DETECTOR_MODEL_NAME": "llama-lab-detector",
+    "RAG_DETECTOR_MODEL_NAME": "rag-detector",
+    "CHECKLIST_EVALUATOR_MODEL_NAME": "checklist-evaluator",
+    "SUMMARY_MODEL_NAME": "summary-model-v2",
+    "MENU_ENGINE_DETECTOR_MODEL_NAME": "menu-detector",
+    "MENU_ENGINE_EVALUATOR_MODEL_NAME": "G35:llama-base__loracpt-4__align-7.2dpo__pref4",
+    "SPEAKER_ENGINE_MODEL_NAME": "llama",
+    "CALL_END_ENGINE_MODEL_NAME": "llama",
+    "TOPIC_DETECTOR_MODEL_NAME": "topic-detector",
+    "MEDICAL_TANGENT_DETECTOR_MODEL_NAME": "medical-tangent-detector",
+    "NON_MEDICAL_TANGENT_DETECTOR_MODEL_NAME": "non-medical-tangent-detector",
+    "SUGGEST_FROM_OTC_LIST": False
+  }
+}
 
-print(url)
+
+config_3 = {
+  "model": "gpt4",
+  "method": "voip",
+  "supportEngines": [
+    "dob-verification-engine",
+    "identity-verification-engine",
+    "speaker-engine",
+    "kickout-engine",
+    "lab-engine",
+    "medication-engine"
+  ],
+  "template": "neel_v2",
+  "asrService": "dg-nova-2-medical",
+  "scriptId": "d06e4d54-6774-495a-818c-eb5ffa30850a",
+  "nurseId": "ed9ca802-fb12-46d8-8298-dae2d2e7f81c",
+  "patientId": "afba5508-6943-4c69-9455-5b4c42fc4ec5",
+  "kickoutId": "b8b2a8be-47ee-4562-bb93-d07ee97ea546",
+  "ttsStreaming": False,
+  "llmStreaming": True,
+  "uiSettings": {
+    "showTasks": True,
+    "showDebug": False,
+    "showSystemPrompts": False,
+    "showVotes": True,
+    "showFeedback": True,
+    "showUserAudio": True
+  },
+  "otherSettings": {
+    "GOD_MODE_ENABLED": True,
+    "USE_LLAMA_SIMPLE_PROMPT_FORMAT": False,
+    "TURN_OFF_CONVERSATION_ENGINE": False,
+    "USE_DYNAMIC_CHEAT_SHEET": False,
+    "USE_SUMMARIZATION": True,
+    "USE_SUMMARY_FOLLOW_UP_IN_KICKOUTS": False,
+    "RAG_EMBEDDING_INDEX_NAME": "facilities-curated",
+    "USE_TOPIC_DETECTION": False,
+    "ELEVEN_LABS_STREAMING_LATENCY": 0,
+    "ELEVEN_LABS_MODEL_ID": "eleven_monolingual_v1",
+    "GOOGLE_ASR_MAX_AGGREGATE": 2,
+    "VOICE_FLOOR_RUN_LOCALLY": True,
+    "VOICE_FLOOR_RUN_TIMEOUT": 0.02,
+    "VOICE_FLOOR_VOLUME": 0,
+    "REMOVE_SPEAKER_UTTERANCE_THRU_VERIFICATION": True,
+    "SPEAKER_VERIFICATION_THRESHOLD_HIGH": 0.02,
+    "SPEAKER_VERIFICATION_THRESHOLD_LOW": 0.01,
+    "SPEAKER_VERIFICATION_NUM_FRAMES": 50,
+    "ASR_CONFIDENCE_STRATEGY": "words",
+    "ASR_CONFIDENCE_THRESHOLD": 0,
+    "SINGLE_WORD_ASR_CONFIDENCE_THRESHOLD": 0.6,
+    "SINGLE_WORD_ASR_CONFIDENCE_THRESHOLD_WHILE_SPEAKING": 0.75,
+    "ENABLE_TTS_CACHE": True,
+    "VAD_THRESHOLD": 0.95,
+    "VAD_CONTENT_BASED_TIME": 2000,
+    "VAD_VERIFICATION_TIME": 250,
+    "DUPLICATE_UTTERANCE_DETECTION_THRESHOLD": 0.9,
+    "GC_KEEP_IN_LAST_N_TURNS": 7,
+    "SUPPRESS_LABMED_DURING_KICKOUTS": False,
+    "SUPPRESS_LABS_AFTER_LAB_SECTION": False,
+    "IDENTITY_ENGINE_MODEL_NAME": "llama",
+    "DOB_ENGINE_MODEL_NAME": "llama-dob-model",
+    "KICKOUT_DETECTOR_MODEL_NAME": "kickout-detector",
+    "KICKOUT_EVALUATOR_MODEL_NAME": "kickout-evaluator",
+    "KICKOUT_FOLLOW_UPS_MODEL_NAME": "kickout-follow-up-evaluator",
+    "MEDICAL_DETECTOR_MODEL_NAME": "llama-medication-detector",
+    "MEDICAL_DOSAGE_EVALUATOR_MODEL_NAME": "llama-medication-dosage-evaluator",
+    "DRUG_ASR_DETECTOR_MODEL_NAME": "llama-drug-asr-detector",
+    "NUTRITION_DETECTOR_MODEL_NAME": "llama-nutrition-detector",
+    "LAB_DETECTOR_MODEL_NAME": "llama-lab-detector",
+    "RAG_DETECTOR_MODEL_NAME": "rag-detector",
+    "CHECKLIST_EVALUATOR_MODEL_NAME": "checklist-evaluator",
+    "SUMMARY_MODEL_NAME": "summary-model-v2",
+    "MENU_ENGINE_DETECTOR_MODEL_NAME": "menu-detector",
+    "MENU_ENGINE_EVALUATOR_MODEL_NAME": "G35:llama-base__loracpt-4__align-7.2dpo__pref4",
+    "SPEAKER_ENGINE_MODEL_NAME": "llama",
+    "CALL_END_ENGINE_MODEL_NAME": "llama",
+    "TOPIC_DETECTOR_MODEL_NAME": "topic-detector",
+    "MEDICAL_TANGENT_DETECTOR_MODEL_NAME": "medical-tangent-detector",
+    "NON_MEDICAL_TANGENT_DETECTOR_MODEL_NAME": "non-medical-tangent-detector",
+    "SUGGEST_FROM_OTC_LIST": False
+  }
+}
+
+
+configs = [config_1, config_2, config_3]
+
+urls = []
+for c in configs:
+    u = url_from_config(c)
+    urls.append({
+        "url": u,
+    })
+
+with open('./make_url_from_config/urls.json', 'w') as f:
+    json.dump(urls, f, indent=4)
